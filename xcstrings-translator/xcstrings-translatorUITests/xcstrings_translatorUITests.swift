6e34abd285e525c59d5f9306301735def87cf048
Wesley de Groot <email@wesleydegroot.nl>
2025-02-09 16:51:56 +0100

    Update xcstrings_translatorUITests.swift

--- xcstrings-translator/xcstrings-translatorUITests/xcstrings_translatorUITests.swift
+++ xcstrings-translator/xcstrings-translatorUITests/xcstrings_translatorUITests.swift
@@ -10,11 +10,7 @@ import XCTest
 final class XCSTRINGSTranslatorUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
     }
 
     override func tearDownWithError() throws { }
@@ -24,8 +20,6 @@ final class XCSTRINGSTranslatorUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     @MainActor
